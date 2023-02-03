bc3bada1614e0b25120920fa690e0d8b5e9aee36
Douglas Ventura <douglasventura.pe@gmail.com>
2023-02-02 21:26:22 -0300

    Passing a Department object to DepartmentForm view

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -51,7 +51,8 @@ public class DepartmentListController implements Initializable {
     @FXML
     public void onBtNewDepartmentAction(ActionEvent event) {
         Stage parentStage = Utils.currentStage(event);
-        createDialogForm("/gui/DepartmentForm.fxml", parentStage);
+        Department department = new Department();
+        createDialogForm(department, "/gui/DepartmentForm.fxml", parentStage);
 
     }
 
@@ -77,10 +78,14 @@ public class DepartmentListController implements Initializable {
         tableViewDepartment.setItems(observableList);
     }
 
-    private void createDialogForm(String absoluteName, Stage parentStage) {
+    private void createDialogForm(Department department, String absoluteName, Stage parentStage) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
             Pane pane = loader.load();
+            
+            DepartmentFormController controller = loader.getController();
+            controller.setDepartment(department);
+            controller.updateFormData();
 
             Stage dialogStage = new Stage();
             dialogStage.setTitle("Enter Department Data");
