a40b0d94c86f8608411063491d51b13cc87bfd3a
Douglas Ventura <douglasventura.pe@gmail.com>
2023-02-03 22:20:56 -0300

    SellerList implemented

---
+++ src/model/services/SellerService.java
@@ -0,0 +1,29 @@
+package model.services;
+
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerService {
+    
+    SellerDao sellerDao = DaoFactory.createSellerDao();
+
+    public List<Seller> findAll() {
+        return sellerDao.findAll();
+    }
+
+    public void saveOrUpdate(Seller seller) {
+        if (seller.getId() == null) {
+            sellerDao.insert(seller);
+        } else {
+            sellerDao.update(seller);
+        }
+    }
+
+    public void removeSeller(Seller seller) {
+        sellerDao.deleteById(seller.getId());
+    }
+
+}
